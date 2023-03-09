21f43bbf82307e2b532fed9a7c5ed49f0047c045
Miguel Arcos <migark@hotmail.com>
2023-03-09 05:32:45 -0500

    Learning02

---
+++ CSE210/prepare/Learning02/Program.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "IT support";
+        job1._company = "Selcomp";
+        job1._startYear = 2010;
+        job1._endYear = 2017;
+
+        Job job2 = new Job();
+        job1._jobTitle = "Technical Sales Consultant";
+        job1._company = "CEMES";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Curriculum myResume = new Curriculum();
+        myResume._name = "Miguel Arcos";
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job1);
+
+        myResume.Display();        
+    }
+}
