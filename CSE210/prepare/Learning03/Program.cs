d9f232431d699b86167792b3f61bbe8946a212c7
Miguel Arcos <migark@hotmail.com>
2023-04-04 18:27:01 -0500

    Learning03

--- CSE210/prepare/Learning03/Program.cs
+++ CSE210/prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetfractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetfractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(3,5);
+        Console.WriteLine(fraction3.GetfractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction fraction4 = new Fraction(7,9);
+        Console.WriteLine(fraction4.GetfractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+        
     }
+    
 }
